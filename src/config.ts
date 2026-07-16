5e7bf71bb86895c03ccee744a31380b193dcfb0e
clina <clina@noreply.codeberg.org>
2026-07-16 07:47:29 +0200

    更新src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -56,6 +56,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		LinkPreset.Friends,
 	],
+  	{
+		name: "开往",
+		url: "https://www.travellings.cn/go.html", // Internal links should not include the base path, as it is automatically added
+		external: true, // Show an external link icon and will open in a new tab
+	},
 };
 
 export const profileConfig: ProfileConfig = {
